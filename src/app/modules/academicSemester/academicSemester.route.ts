1b3a76185403e844782e99ba491f504b4e8e4542
Farvez Hossen <56191538+Farvez999@users.noreply.github.com>
2023-08-23 00:39:42 +0600

    Functionality with catchAsync and Validation

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicSemesterValidation } from './academicSemester.validation';
 import { AcademicSemesterController } from './academicSemesterController';
 const router = express.Router();
 
-router.post('/', AcademicSemesterController.insertIntoDB);
+router.post(
+  '/',
+  validateRequest(AcademicSemesterValidation.create),
+  AcademicSemesterController.insertIntoDB
+);
 
 export const AcademicSemesterRoutes = router;
